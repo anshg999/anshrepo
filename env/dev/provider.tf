ebe1cc055d0148c3e39cdd9c17bb0ad5318481d7
anshg999 <anshgupta18.ag@gmail.com>
2025-09-08 02:49:12 +0530

    Update provider.tf

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -10,6 +10,7 @@ terraform {
     storage_account_name = "ansh456"
     container_name       = "anshcontainer"
     key                  = "dev.terraform.tfstate"
+    subscription_id = "ef88ee82-ddb3-44a3-970a-7a8e3296f3f5"
     
   }
 }
